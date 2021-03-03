a172b2b9613d9c9226efb963e46346ab68799d6e
rozzyy <rozzyrahmanda0@gmail.com>
2021-03-03 17:29:31 +0700

    update portofolio

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,70 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        personal-portofolio
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <div>
+    <!-- navbar -->
+     <b-navbar toggleable="lg" type="light" variant="transparent" class="container">
+      <b-navbar-brand href="#" class="font-weight-bold" style="font-size: 30px">rrahmanda</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+           <b-navbar-nav>
+            <b-nav-item href="#" active class="font-weight-bold">HOME</b-nav-item>
+            <b-nav-item href="#" class="font-weight-bold">ABOUT </b-nav-item>
+            <b-nav-item href="#" class="font-weight-bold">SKILLS</b-nav-item>
+            <b-nav-item href="#" class="font-weight-bold">PROJECTS</b-nav-item>
+            <b-nav-item href="#" class="font-weight-bold">CONTACT</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+    <div class="container">
+     <b-row>
+       <b-col style="margin-top: 200px;" sm="7">
+          <h5>Hello! I Am Rozzy Rahmanda</h5>
+          <h2 class="font-weight-bold">I AM &lt; FULLSTACK DEVELOPER /&gt;</h2>
+          <p>
+            I'm a Web and Android Developer based in Medan. I build web and mobile apps with beautiful and cool design.
+          </p>
+          <b-row>
+            <b-col>
+              <b-button variant="outline-dark" class="container">Download CV</b-button>
+            </b-col>
+            <b-col>
+              <b-button variant="outline-dark" class="container">Hire Me!</b-button>
+            </b-col>
+          </b-row>
+       </b-col>
+       <b-col>
+         <div class="border border-dark">
+           Ilustrasi
+         </div>
+       </b-col>
+     </b-row>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import image from '@/assets/back-image.jpg'
+export default {
+  data () {
+    return {
+      gambar: image
+    }
+  }
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+<style scoped>
+.back-image {
+  background: url('~assets/back-image.jpg') rgb(61, 59, 59, 0.8);
+  background-size: cover;
+  background-blend-mode: multiply;
+  height: 100vh;
 }
-
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+body, html {
+  height: 100%;
 }
 </style>
